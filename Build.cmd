94448ef2947a9c2ca00936f2841ad731984d3409
Achim Kuntz <achimobil@hotmail.com>
2024-11-30 09:46:14 +0100

    Add Commands

---
+++ Build.cmd
@@ -0,0 +1,1 @@
+"C:\Program Files\7-Zip\7z.exe" u -tzip "FS25_InfoDisplayExtension.zip" -x@FileDefinition.txt
---
+++ Test25.Modtest.1.cmd
@@ -0,0 +1,2 @@
+"C:\Program Files\7-Zip\7z.exe" u -tzip "E:\LS25Profile\LS25ModTest\mods\FS25_InfoDisplayExtension.zip" -x@FileDefinition.txt
+start "" "C:\Program Files (x86)\Farming Simulator 2025\x64\FarmingSimulator2025Game.exe" -autoStartSavegameId 1 -cheats -profile E:\LS25Profile\LS25ModTest -devWarning
