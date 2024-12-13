ae3aa6e2ad65f50d3dae86368bc19cdbe742d050
Achim Kuntz <achimobil@hotmail.com>
2024-12-13 08:25:31 +0100

    typo fixed #23

--- InfoDisplayExtension.lua
+++ InfoDisplayExtension.lua
@@ -862,7 +862,7 @@ Weitere informationen zu Bäumen anzeigen.]]
 
             -- alter in Stunden mit Angabe des maximal alters
             local ageText = g_i18n:getText("infohud_fullGrown");
-            if foundTree.growthStateI ~= 1 and foundTree.isGrowing == true and foundTree.nextGrowthTargetHour ~= nilthen
+            if foundTree.growthStateI ~= 1 and foundTree.isGrowing == true and foundTree.nextGrowthTargetHour ~= nil then
                 local numOfGrowStates = table.getn(treeTypeDesc.stages);
                 local totalGrowHours = (treeTypeDesc.growthTimeHours * (numOfGrowStates - 1)) / g_currentMission.environment.timeAdjustment;
                 local hoursNow = ((g_currentMission.environment.currentDay - 1) * 24 ) + g_currentMission.environment.currentHour;
